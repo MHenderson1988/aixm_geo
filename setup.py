639f6740eebb2d9e1b179f0c8a74327e40acd0c9
Mark Henderson <mark.henderson1988@gmail.com>
2023-06-19 12:03:20 +0100

    Added setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="AIXMGeo",
+    version="0.0.1 Alpha",
+    author="Mark Henderson",
+    author_email="mark.henderson1988@gmail.com",
+    description="A Python module which wraps KMLPlus to produce kml files from AIXM 5.1 geographical data.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MHenderson1988/aixm_geo",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
