6b2f91c3ba8d23a9868a50f861a673348010c9ef
Mark Henderson <mark.henderson1988@gmail.com>
2023-06-23 17:54:24 +0100

    Requirements updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="AIXMGeo",
-    version="0.0.1 Alpha",
+    version="0.0.2",
     author="Mark Henderson",
     author_email="mark.henderson1988@gmail.com",
     description="A Python module which wraps KMLPlus to produce kml files from AIXM 5.1 geographical data.",
